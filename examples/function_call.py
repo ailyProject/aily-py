b34333b671b204bb7147b60cf776900134156e1f
stao <werewolf_st@hotmail.com>
2024-05-24 14:00:24 +0800

    test

--- examples/function_call.py
+++ examples/function_call.py
@@ -1,10 +1,24 @@
+import base64
+
 from aily import AIGC
 from aily.tools import speex_decoder, speech_to_text, text_to_speech
 from loguru import logger
 
 
+def encode_image(image_path):
+    with open(image_path, "rb") as image_file:
+        encoded_string = base64.b64encode(image_file.read()).decode("utf-8")
+    return encoded_string
+
+
 def function_call_handler(event):
-    logger.info(event)
+    logger.info("收到函数调用请求：{0}".format(event))
+    call_id = event["id"]
+    if event["name"] == "get_picture":
+        logger.debug("调用摄像头拍照")
+        
+        image = encode_image("./vision_test_image.jpg")
+        aigc.reply_message(call_id, image, "image")
 
 
 def record_end_handler(data):
@@ -23,21 +37,18 @@ def invoke_end_handler(data):
 tools = [{
     "type": "function",
     "function": {
-        "name": "test_function",
-        "description": "This is a test function",
+        "name": "get_picture",
+        "description": "Get a picture from Camera",
         "parameters": {
             "type": "object",
             "properties": {
-                "param1": {
-                    "type": "string",
-                    "description": "This is the first parameter"
-                },
-                "param2": {
-                    "type": "string",
-                    "description": "This is the second parameter"
-                },
+                "switch": {
+                    "type": "boolean",
+                    "enum": [True, False],
+                    "description": "是否打开摄像头",
+                }
             },
-            "required": ["param1", "param2"]
+            "required": ["switch"],
         }
     }
 }]
@@ -49,6 +60,6 @@ aigc.on_invoke_end.subscribe(lambda i: invoke_end_handler(i))
 aigc.on_function_call.subscribe(lambda i: function_call_handler(i))
 
 aigc.register_tools(tools)
-aigc.choice_tool("test_function")
+aigc.choice_tool("get_picture")
 
 aigc.run()
